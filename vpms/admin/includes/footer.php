57d596b2523ec22c11bb1c8b663f36db653464a1
tariqmalik <Muhammad.tariq@itgs.pk>
2023-05-08 23:21:46 +0500

    first commit

---
+++ vpms/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="site-footer">
+            <div class="footer-inner bg-black">
+                <div class="row">
+                   
+                    <div style="background-color: gray; padding-top:8px; margin-top:180px ; position:fixed; margin-left:-30px;" class="col-sm-12 text-center">
+                       <p style="color:#ffffff;"> Vehicle Parking Management System @2023</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
---
+++ vpms/users/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-black">
+                <div class="row">
+                   
+                    <div style="background-color: gray; padding-top:8px; margin-top:370px ; " class="col-sm-12 text-center">
+                       <p style="color:#ffffff;"> Vehicle Parking Management System @2023</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
